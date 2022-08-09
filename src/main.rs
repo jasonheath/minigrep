90d51f554f93b24aa009ad1aec4db8852002de37
Jason Heath <jh@jasonheath.com>
2022-08-09 12:56:59 -0400

    introducing a parse_config function

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,9 @@ use std::env;
 use std::fs;
 
 fn main() {
-    // --snip--
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let filename = &args[2];
+    let (query, filename) = parse_config(&args);
 
     println!("Searching for {}", query);
     println!("In file {}", filename);
@@ -15,4 +13,11 @@ fn main() {
         .expect("Something went wrong reading the file");
 
     println!("With text:\n{}", contents);
+}
+
+fn parse_config(args: &[String]) -> (&str, &str) {
+    let query = &args[1];
+    let filename = &args[2];
+
+    (query, filename)
 }
