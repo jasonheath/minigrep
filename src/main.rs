2aa0f4ba6609e465ef9e78cafb811b2db903df8d
Jason Heath <jh@jasonheath.com>
2022-08-09 11:41:54 -0400

    This is a second comment just artificially introducing changes

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::env;
 
 // this is a comment to force a change
+// this is a second comment just to introduce a change into the source code
 fn main() {
     let args: Vec<String> = env::args().collect();
     println!("{:?}", args);
